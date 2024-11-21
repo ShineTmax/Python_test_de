390c212c7620f55d3542f65639e1db2769038d20
Sid CHORFI <chorfisidali@gmail.com>
2024-11-21 18:41:58 +0100

    Update test_data_reading.py

--- Python/tests/test_data_reading.py
+++ Python/tests/test_data_reading.py
@@ -1,22 +1,59 @@
-import unittest
-from src.data_reading import read_csv_file, read_json_file
-from pathlib import Path
 import pandas as pd
-from tests import INPUT_DRUGS_TEST_PATH, INPUT_PUBMED_JSON_TEST_PATH
+import chardet
+import json
+from src import logger
+from pathlib import Path
 
-class TestDataReading(unittest.TestCase):
 
-    def test_read_csv_file(self):
-        file_path = Path(INPUT_DRUGS_TEST_PATH)
-        data = read_csv_file(file_path)
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assertFalse(data.empty)
+def read_csv_file(file_path: Path) -> pd.DataFrame:
+    """
+    Read a CSV file and return it as a DataFrame.
+    Args:
+        file_path (Path): Path to the CSV file.
+    Returns:
+        pd.DataFrame: CSV data as DataFrame, or empty DataFrame if an error occurs.
+    """
+    try:
+        data = pd.read_csv(file_path, encoding="utf-8")
+        return data
+    except Exception as e:
+        logger.error(f"An error occurred while reading '{file_path}': {e}")
+        return pd.DataFrame()  # Return empty DataFrame on error
 
-    def test_read_json_file(self):
-        file_path = Path(INPUT_PUBMED_JSON_TEST_PATH)
-        data = read_json_file(file_path)
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assertFalse(data.empty)
 
-if __name__ == '__main__':
-    unittest.main()
+def read_json_file(file_path: Path) -> pd.DataFrame:
+    """
+    Read a JSON file and return it as a DataFrame.
+    Args:
+        file_path (Path): Path to the JSON file.
+    Returns:
+        pd.DataFrame: JSON data as DataFrame, or empty DataFrame if an error occurs.
+    """
+    try:
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        return pd.DataFrame(data)
+    except Exception as e:
+        logger.error(f"An error occurred while reading '{file_path}': {e}")
+        return pd.DataFrame()  # Return empty DataFrame on error
+
+
+def read_data(input_files: list[Path]) -> list[pd.DataFrame]:
+    """
+    Read a list of CSV and JSON files into DataFrames.
+    Args:
+        input_files (list[Path]): List of file paths to read.
+    Returns:
+        list[pd.DataFrame]: List of DataFrames corresponding to the input files.
+    """
+    logger.info("Input files: %s", input_files)
+    data = []
+    for file_path in input_files:
+        if file_path.suffix == ".csv":
+            df = read_csv_file(file_path)
+        elif file_path.suffix == ".json":
+            df = read_json_file(file_path)
+        else:
+            continue
+        data.append(df)
+    return data
