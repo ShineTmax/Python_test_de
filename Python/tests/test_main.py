b99ff22003fe6914668d86fd88c2825116fd5caa
Sid CHORFI <chorfisidali@gmail.com>
2024-11-21 18:42:54 +0100

    Update test_main.py

--- Python/tests/test_main.py
+++ Python/tests/test_main.py
@@ -2,14 +2,37 @@ import unittest
 from main import main
 
 class TestMain(unittest.TestCase):
+    """
+    Unit test class for testing the `main` function.
+
+    This class tests the `main` function by executing it and ensuring that it runs
+    without raising any exceptions. The test checks that the main application logic 
+    completes successfully.
+
+    Methods:
+        test_main: Tests the execution of the `main` function by asserting that it
+                   runs without any exceptions.
+    """
 
     def test_main(self):
+        """
+        Test the execution of the `main` function.
+
+        This test attempts to run the `main` function and ensures that no exceptions
+        are raised during its execution. If an exception occurs, it is caught, and the test
+        will fail.
+
+        Asserts:
+            - The `main` function runs without raising any exceptions.
+        """
         try:
             main()
             success = True
         except Exception as e:
             success = False
             print(e)
+        
+        # Assert that the main function executed successfully without exceptions
         self.assertTrue(success)
 
 if __name__ == '__main__':
